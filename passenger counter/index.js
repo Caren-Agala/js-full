3c1e118ecbe758a565fc7d2015b432a11f068fcb
Caren-Agala <caren.khavukui@students.jkuat.ac.ke>
2023-02-02 15:29:09 +0300

    using functions to write less code

--- passenger counter/index.js
+++ passenger counter/index.js
@@ -7,3 +7,16 @@
 function increment() {
     console.log("button was clicked")
 }
+
+
+function countdown() {
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+}
+
+countdown()
+countdown()
+ increment()
