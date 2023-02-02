4c8275b7eb79e056c4dbeb708cad76ace5d1dfa5
Caren-Agala <caren.khavukui@students.jkuat.ac.ke>
2023-02-02 21:11:52 +0300

    js strings

--- passenger counter/index.js
+++ passenger counter/index.js
@@ -15,3 +15,26 @@ function increment() {
 function save() {
     console.log(count)
 }
+
+let usernames = "John"
+
+let message = "You have three notifications"
+
+console.log(message + ", " + usernames + "!")
+
+var messageToUser
+messageToUser = message + ", " + usernames + "!"
+
+console.log(messageToUser)
+
+
+var name = "Caren"
+var greeting = "Hi, my name is "
+
+
+console.log(greeting + " " + name)
+
+var myGreeting = greeting + " " + name
+console.log(myGreeting)
+
+
