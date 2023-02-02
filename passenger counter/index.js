bec6572152812a8d53909836893ec19f7fa255b5
Caren-Agala <caren.khavukui@students.jkuat.ac.ke>
2023-02-02 13:49:38 +0300

    js for my age

--- passenger counter/index.js
+++ passenger counter/index.js
@@ -1,2 +1,11 @@
 let count = 0;
-let saveEl = document.get
+
+console.log(count);
+// let saveEl = document.get
+// document.getElementById("count-el").innerText = 5
+
+var myAge
+let today = new Date()
+myAge = 22
+console.log(myAge) , 
+console.log(today)
